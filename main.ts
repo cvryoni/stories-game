bce0493ae0ce08371b44ff24fce1ff77a751672d
cvryoni <121550180+cvryoni@users.noreply.github.com>
2023-12-29 12:13:29 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    music.play(music.stringPlayable("F G A B C5 A F G ", 230), music.PlaybackMode.UntilDone)
+    Category = randint(0, 3)
+    for (let index = 0; index < 2; index++) {
+        basic.showString("" + (Messages[Category]))
+    }
+})
+let Category = 0
+let Messages: string[] = []
+Messages = [
+"Action",
+"Fantasy",
+"Comedy",
+"Horror"
+]
